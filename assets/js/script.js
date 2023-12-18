2fa0870ea67992af995a5ef29dc50dfc412a05ad
Chantal Cassini-Jones <ccassinijones@gmail.com>
2023-12-18 20:58:14 +0000

    Refactor: for loop add to simplify code

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,16 +35,11 @@ $('.saveBtn').on('click', function(event) {
 	localStorage.setItem(time, text);
 });
 
-//Persist events between refreshes of a page 
-$(" #hour-9 .description").val(localStorage.getItem("hour-9"));
-$(" #hour-10 .description").val(localStorage.getItem("hour-10"));
-$(" #hour-11 .description").val(localStorage.getItem("hour-11"));
-$(" #hour-12 .description").val(localStorage.getItem("hour-12"));
-$(" #hour-13 .description").val(localStorage.getItem("hour-13"));
-$(" #hour-14 .description").val(localStorage.getItem("hour-14"));
-$(" #hour-15 .description").val(localStorage.getItem("hour-15"));
-$(" #hour-16 .description").val(localStorage.getItem("hour-16"));
-$(" #hour-17 .description").val(localStorage.getItem("hour-17"));
+//Persist events between refreshes of a page
+// for loop to iterate over all the time-block textarea and get their value from local storage when page is refreshed 
+for (let i = 9; i <= 17; i++) {
+    $("#hour-" + i + " .description").val(localStorage.getItem("hour-" + i));
+};
 
 colourCode();
 
