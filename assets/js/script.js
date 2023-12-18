bfc01d40c2b58ed1f2d5574f817cdb0ae405741b
Chantal Cassini-Jones <ccassinijones@gmail.com>
2023-12-18 17:55:18 +0000

    Feat: localStorage.getItem add for each time block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,10 @@
-//Display the current day at the top of the calender when a user opens the planner. done
- 
-//Present timeblocks for standard business hours when the user scrolls down. done
+
  
 //Color-code each timeblock based on past, present, and future when the timeblock is viewed.
  
 //Allow a user to enter an event when they click a timeblock. done
 
-// Save the event in local storage when the save button is clicked in that timeblock.
 
-//Persist events between refreshes of a page
-
-//Uses a date utility library to work with date and time. done 
-
-//ensures that jQuery code runs after the DOM is fully loaded. done
 $(function() {
 
 var today = dayjs();
@@ -23,18 +15,27 @@ $('#currentDay').text(today.format('dddd, MMMM DD'));
 
 
 
-//event listener when save button is clicked and save to local storage
+//event listener when save button is clicked 
 
 $('.saveBtn').on('click', function(event) {
     event.preventDefault(); 
+
+//get value from textarea 
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
+    //save to local storage
 	localStorage.setItem(time,text);
 });
 
-//saving event in local storage 
-//$()
+//Persist events between refreshes of a page 
 
-
-
+$(" #hour-9 .description").val(localStorage.getItem("hour-9"));
+$(" #hour-10 .description").val(localStorage.getItem("hour-10"));
+$(" #hour-11 .description").val(localStorage.getItem("hour-11"));
+$(" #hour-12 .description").val(localStorage.getItem("hour-12"));
+$(" #hour-1 .description").val(localStorage.getItem("hour-1"));
+$(" #hour-2 .description").val(localStorage.getItem("hour-2"));
+$(" #hour-3 .description").val(localStorage.getItem("hour-3"));
+$(" #hour-4 .description").val(localStorage.getItem("hour-4"));
+$(" #hour-5 .description").val(localStorage.getItem("hour-5"));
 });
